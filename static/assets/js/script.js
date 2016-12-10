39007ce20303752f2dcacb2607469b74cca1e8db
shejza <shejzaberisha@gmail.com>
2016-12-10 14:40:36 +0100

    requirments

---
+++ static/assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+function yesnoCheck() {
+    if (document.getElementById('yesCheck').checked) {
+        document.getElementById('ifYes').style.visibility = 'visible';
+    }
+    else document.getElementById('ifYes').style.visibility = 'hidden';
+}
+
+function yesnoCheck2() {
+    if (document.getElementById('yesCheck2').checked) {
+        document.getElementById('ifYes2').style.visibility = 'visible';
+    }
+    else document.getElementById('ifYes2').style.visibility = 'hidden';
+}
